83b70b44b5c5c58566e5ef63854abfceeaff16b9
Gábor Csárdi <csardi.gabor@gmail.com>
2016-06-03 13:56:13 +0100

    Read values from file(s)

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(shiny)
 
 ui <- basicPage(
   fluidRow(
+    selectInput("inputFile", "File", choices = c("x.txt", "y.txt")),
     actionButton(inputId = "add_button", label = "Add Button")
   ),
   uiOutput("more_buttons")
@@ -22,11 +23,42 @@ server <- function(input, output){
   )
 
   observeEvent(
+    input$inputFile,
+    {
+      rvs$buttons <- list()
+      btn <- readLines(input$inputFile)
+      for (str in btn) {
+        id <- random_id()
+        rvs$buttons[[id]] <- div(
+          actionButton(inputId = paste0("button", id), label = str),
+          actionButton(
+            inputId = paste0("del_button", id),
+            label = paste0("Delete")
+          )
+        )
+
+        local({
+          id2 <- id
+          observeEvent(
+            input[[paste0("button", id2)]],
+            { print(id2) }
+          )
+
+          observeEvent(
+            input[[paste0("del_button", id2)]],
+            { rvs$buttons[[id2]] <- NULL }
+          )
+        })
+      }
+    }
+  )
+
+  observeEvent(
     input$add_button,
     {
       id <- random_id()
       rvs$buttons[[id]] <- div(
-        actionButton(inputId = paste0("button", id), label = id),
+        actionButton(inputId = paste0("button", id), label = "xxx"),
         actionButton(
           inputId = paste0("del_button", id),
           label = paste0("Delete")
